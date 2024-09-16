0ecd631c5572d06b0f3845ea3b0615bcce7873cb
PiratePenguin2 <PiratePenguin2.2649@gmail.com>
2024-09-16 11:00:52 -0700

    Minor Reformatting
    Optimized getButton#() functions
    Fixed minor syntax errors
    Moved all functions below #declarations and above variables, setup and main loop.

--- Altimeter_1.0.0/Altimeter_1.0.0.ino
+++ Altimeter_1.0.0/Altimeter_1.0.0.ino
@@ -15,67 +15,11 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-
 #define BUTTON_1_PIN 1
 #define BUTTON_2_PIN 2
 #define BUTTON_3_PIN 3
 #define BUTTON_4_PIN 4
 
-int id = 0;
-bool menuActive = false;
-
-void setup() {
-  Serial.begin(9600);
-
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-  display.display();
-  display.clearDisplay();
-
-  testscrolltext();    // Draw scrolling text
-
-  pinMode(BUTTON_1_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_2_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_3_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_4_PIN, INPUT_PULLUP);
-
-
-}
-
-void loop() {
-
-  if (getButton1()) {
-    if (menuActive) {
-      menuActive = false;
-      while (getButton1())
-    } else {
-      menyActive = true;
-      while (getButton1());
-    }
-  }
-
-  if (menuActive) {
-    if (getButton2()) {
-      if (id >= 3) {}
-        id = 0;
-      } else {
-        id += 1;
-      }
-    } 
-    while (getButton2()) {}
-    displayMenu(id, true, true);
-  } else {
-    //do something else, show static page
-  }
-
-
-
-  
-
-}
 
 void testscrolltext(void) {
   display.clearDisplay();
@@ -127,41 +71,75 @@ void displayMenu(int id, bool clear=false, bool update=false) {
 }
 
 bool getButton1() {
-  bool state;
-  if (digitalRead(BUTTON_1_PIN) == HIGH) {
-    state = true;
-  } else {
-    state = false;
-  }
-  return state;
+  bool state = digitalRead(BUTTON_1_PIN == HIGH);
+  return state ? true : false;
 }
 bool getButton2() {
-  bool state;
-  if (digitalRead(BUTTON_2_PIN) == HIGH) {
-    state = true;
-  } else {
-    state = false;
-  }
-  return state;
+  bool state = digitalRead(BUTTON_2_PIN == HIGH);
+  return state ? true : false;
 }
 bool getButton3() {
-  bool state;
-  if (digitalRead(BUTTON_3_PIN) == HIGH) {
-    state = true;
-  } else {
-    state = false;
-  }
-  return state;
+  bool state = digitalRead(BUTTON_3_PIN == HIGH);
+  return state ? true : false;
 }
 bool getButton4() {
-  bool state;
-  if (digitalRead(BUTTON_4_PIN) == HIGH) {
-    state = true;
-  } else {
-    state = false;
-  }
-  return state;
+  bool state = digitalRead(BUTTON_4_PIN == HIGH);
+  return state ? true : false;
 }
 
 
+int id = 0;
+bool menuActive = false;
 
+void setup() {
+  Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+  display.clearDisplay();
+
+  testscrolltext();    // Draw scrolling text
+
+  pinMode(BUTTON_1_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_2_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_3_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_4_PIN, INPUT_PULLUP);
+
+
+}
+
+void loop() {
+
+  if (getButton1()) {
+    if (menuActive) {
+      menuActive = false;
+      while (getButton1()) {}
+    } else {
+      menuActive = true;
+      while (getButton1()) {}
+    }
+  }
+
+  if (menuActive) {
+    if (getButton2()) {
+      if (id >= 3) {
+        id = 0;
+      } else {
+        id += 1;
+      }
+    } 
+    while (getButton2()) {}
+    displayMenu(id, true, true);
+  } else {
+    //do something else, show static page
+  }
+
+
+
+  
+
+}
