d3b474691d43aa1f684ede778eb5917cbcf3ef0e
chetfrankjo <99208343+chetfrankjo@users.noreply.github.com>
2024-09-30 19:20:15 -0700

    disable splashscreen

--- Altimeter_1.0.1/Altimeter_1.0.1.ino
+++ Altimeter_1.0.1/Altimeter_1.0.1.ino
@@ -38,7 +38,7 @@ const char* password = "L@ncerN@tion"; // Replace with your Wi-Fi password
 
 #define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET, false);
 
 #define REC_BLINK_DELAY 650
 #define REC_LIVE_INTERVAL 500
